abf476cf41dc371cf2c02fbfac06bb7972d3434d
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-11-20 15:04:11 -0300

    agregue un bucle para el ingreso de los prestamos

--- eje4.c
+++ eje4.c
@@ -19,7 +19,7 @@ typedef struct
     Fecha fechPubli;
     int id;
     int prestamos[12];
-    char **historialPrestamos;
+    int **historialUsuarios;
 } Libro;
 
 void cargarLibro(Libro *librito);
@@ -39,15 +39,27 @@ void cargarLibro(Libro *librito) {
     scanf("%d", &librito->id);
     printf("Ingrese la cantidad de prestamos que tuvo: \n");
     scanf("%d", &librito->id);
-    // redimenciono el arreglo dinamico
-    //librito->historialPrestamos = (char **)realloc(librito->historialPrestamos, librito->id * (char **));
-    for (int i = 0; i < librito->id; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("Ingrese el mes de prestamo: \n");
-            fflush(stdin);
+    // cargo los prestamos mensuales
+    printf("Ingrese la cantidad de préstamos mensuales para cada mes:\n");
+    for (int i = 0; i < 12; ++i) {
+        printf("Mes %d: ", i + 1);
+        scanf("%d", &librito->prestamos[i]);
+    }
+    // cargar información del historiaal de usuarios
+    librito->historialUsuarios = (int **)malloc(12 * sizeof(int *));
+    for (int i = 0; i < 12; ++i) {
+        librito->historialUsuarios[i] = (int *)malloc(50 * sizeof(int));
+        for (int j = 0; j < 50; ++j) {
+            librito->historialUsuarios[i][j] = 0;
         }
     }
 }
 int main() {
+    Libro librito1;
+    cargarLibro(&librito1);
+    for (int i = 0; i < 12; ++i) {
+        free(librito1.historialUsuarios[i]);
+    }
+    free(librito1.historialUsuarios);
     return 0;
 }
